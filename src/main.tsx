55bbe375ff60161783f06924fceecb181835a901
Arthur <arthurferreira1417@gmail.com>
2024-11-01 12:08:49 -0300

    Feat: creating routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Global } from "./global.tsx";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/login/";
+import Home from "./pages/home/";
+import Error from "./pages/error/";
+import { ThemeContextProvider } from "./theme/provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/home/",
+    element: <Home />,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <ThemeContextProvider>
+    <Global />
+    <RouterProvider router={router} />
+  </ThemeContextProvider>
+);
