584f56d12ec19a8c2833f76a36c07b0f4824483c
Arthur <arthurferreira1417@gmail.com>
2024-11-02 18:06:11 -0300

    Feat: adding style in login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,31 @@ export default function Login() {
   return (
     <main className="w-full h-screen bg-primary100 flex items-center justify-center">
       <section className="flex flex-row items-start justify-between w-11/12 h-4/5">
-        <aside className=" w-4/12 h-full">
-          <header className="text-center md:text-left">
-            <Image
-              src="/LogoGrenke.webp"
-              alt="Logo da Grenke"
-              width={100}
-              height={100}
-              layout="responsive"
-            />
-            <h1 className="text-lg font-semibold mt-4">
+        <aside className=" flex flex-col w-4/12 h-full align-top justify-between">
+          <header className=" text-left w-3/4">
+            <div className=" relative w-[150px] h-[50px]">
+              <Image
+                src="/LogoGrenke.webp"
+                alt="Logo da Grenke"
+                layout="fill"
+                objectFit="contain"
+                priority={true}
+              />
+            </div>
+            <h1 className=" text-lg text-start py-3 text-white font-light ">
               Bem-vindo de volta! Faça login para continuar.
             </h1>
           </header>
 
-          <form className="space-y-6">
+          <form className="flex flex-col align-top space-y-6 w-3/4">
             <fieldset>
               <legend className="sr-only">Formulário de Login</legend>
 
               <div>
-                <label htmlFor="email" className="block text-sm font-medium">
+                <label
+                  htmlFor="email"
+                  className=" text-base text-white font-regular"
+                >
                   Email
                 </label>
                 <input
@@ -32,11 +37,14 @@ export default function Login() {
                   placeholder="email@gmail.com"
                   className="w-full mt-1 p-2 border rounded"
                 />
-                <p className="text-xs text-red-500 mt-1">Email inválido</p>
+                <p className="text-xs text-red100 mt-1">Email inválido</p>
               </div>
 
               <div className="mt-4">
-                <label htmlFor="password" className="block text-sm font-medium">
+                <label
+                  htmlFor="password"
+                  className="text-base text-white font-regular"
+                >
                   Senha
                 </label>
                 <input
@@ -45,23 +53,23 @@ export default function Login() {
                   placeholder="sua senha"
                   className="w-full mt-1 p-2 border rounded"
                 />
-                <p className="text-xs text-red-500 mt-1">Senha inválida</p>
+                <p className="text-xs text-red100 mt-1">Senha inválida</p>
               </div>
             </fieldset>
 
             <button
               type="submit"
-              className="w-full bg-blue-600 text-white p-2 rounded font-semibold"
+              className="w-full bg-blue270 text-white  p-2 rounded font-semibold"
             >
               Entrar
             </button>
           </form>
-          <footer className="text-center mt-4 text-xs text-gray-500">
+          <footer className="text-center  align-bottom mt-auto h-auto w-3/4  text-xs text-gray300">
             ℗ 2024 Arthur Ferreira, All rights reserved.
           </footer>
         </aside>
 
-        <figure className="relative w-7/12 h-full">
+        <figure className="relative w-8/12 h-full">
           <Image
             src="/LoginImage.webp"
             alt="Mulher segurando um notebook"
