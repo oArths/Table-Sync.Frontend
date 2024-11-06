1fe3c40208a54333c1426f0c928bb396e38a8f2b
Arthur <arthurferreira1417@gmail.com>
2024-11-06 14:52:49 -0300

    Feat: creating validation to  header render  just in home page

--- src/app/template/header/index.tsx
+++ src/app/template/header/index.tsx
@@ -7,42 +7,40 @@ import { useState } from "react";
 export const Header = () => {
   const [menu, setMenu] = useState(false);
   let path = usePathname();
-  if (path === "/") {
-    return;
+  if (path === "/home") {
+    return (
+      <header className="flex items-center justify-center w-full h-16   bg-primary100 border border-solid  border-b-gray100/50  ">
+        <div className="w-11/12  h-full flex flex-row items-center justify-between ">
+          <div className=" w-[100px] h-[30px]">
+            <SuspenseImage
+              src="/LogoGrenke.webp"
+              alt="Logo da Grenke"
+              width={100}
+              height={30}
+              priority={true}
+            />
+          </div>
+          <div
+            onClick={() => setMenu(!menu)}
+            className="cursor-pointer flex items-center justify-center rounded-full border border-solid border-primary200  h-11 w-11 bg-primary100"
+          >
+            <SuspenseImage
+              src="/UserIcon.svg"
+              alt="Logo da Grenke"
+              width={25}
+              className="rounded-full"
+              height={25}
+              priority={true}
+            />
+          </div>
+        </div>
+        <Menu
+          name="Janaina Mendes"
+          position="Grenke"
+          close={() => setMenu(false)}
+          open={menu}
+        />
+      </header>
+    );
   }
-
-  return (
-    <header className="flex items-center justify-center w-full h-16   bg-primary100 border border-solid  border-b-gray100/50  ">
-      <div className="w-11/12  h-full flex flex-row items-center justify-between ">
-        <div className=" w-[100px] h-[30px]">
-          <SuspenseImage
-            src="/LogoGrenke.webp"
-            alt="Logo da Grenke"
-            width={100}
-            height={30}
-            priority={true}
-          />
-        </div>
-        <div
-          onClick={() => setMenu(!menu)}
-          className="cursor-pointer flex items-center justify-center rounded-full border border-solid border-primary200  h-11 w-11 bg-primary100"
-        >
-          <SuspenseImage
-            src="/UserIcon.svg"
-            alt="Logo da Grenke"
-            width={25}
-            className="rounded-full"
-            height={25}
-            priority={true}
-          />
-        </div>
-      </div>
-      <Menu
-        name="Janaina Mendes"
-        position="Grenke"
-        close={() => setMenu(false)}
-        open={menu}
-      />
-    </header>
-  );
 };
