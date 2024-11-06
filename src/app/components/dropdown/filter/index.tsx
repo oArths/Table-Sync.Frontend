2fe0affce153184bd24af95aa4206d5f1b059c30
Arthur <arthurferreira1417@gmail.com>
2024-11-05 21:16:03 -0300

    Feat: create a filter componente and use

---
+++ src/app/components/dropdown/filter/index.tsx
@@ -0,0 +1,58 @@
+import * as I from "lucide-react";
+interface IFilter {
+  open: boolean;
+  options: string[];
+  selectedOptions: string[];
+  onClick: () => void;
+  onSelect: (selected: string[]) => void;
+}
+
+export default function Filter({
+  options,
+  open,
+  selectedOptions,
+  onClick,
+  onSelect,
+}: IFilter) {
+  const handleOptionClick = (value: string) => {
+    const isSelected = selectedOptions.includes(value);
+    const newSelectedOptions = isSelected
+      ? selectedOptions.filter((option) => option !== value)
+      : [...selectedOptions, value];
+    onSelect(newSelectedOptions);
+  };
+  return (
+    <div className="relative  w-32 h-10  ">
+      <p
+        onClick={onClick}
+        className="cursor-pointer select-none font-medium text-base text-gray500 px-4 flex flex-row items-center justify-between w-full h-full bg-primary200 rounded"
+      >
+        Status
+        <I.ChevronDown widths="24px" height="24px" color="rgb(144, 144, 144)" className={ open ? ` bg-red100` : ''} />
+      </p>
+      {open && (
+        <div className="absolute flex flex-col items-center justify-center py-2 z-10 w-32 mt-1 bg-primary200 rounded">
+          {options.map((value, index) => {
+            const isSelected = selectedOptions.includes(value);
+            return (
+              <p
+                key={index}
+                onClick={() => handleOptionClick(value)}
+                className="flex flex-row items-center px-2 font-regular text-base text-gray500 justify-between w-5/6 rounded-sm hover:bg-primary400 cursor-pointer select-none"
+              >
+                {value}
+                {isSelected && (
+                  <I.Check
+                    width="15px"
+                    height="15px"
+                    color="rgb(212, 212, 212)"
+                  />
+                )}
+              </p>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,53 @@
+"use client";
+import { useState } from "react";
+import { Button } from "../components/button";
+import Filter from "../components/dropdown/filter";
 export default function Home() {
+  const [filter, setFilter] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+  const data = ["KU", "KA", "KL", "KD"];
+
+  const handleSelect = (selected: string[]) => {
+    setSelectedOptions(selected);
+  };
+
   return (
-      <div className="flex-col  w-full h-screen-minus-80 items-start bg-primary100 justify-items-center  ">
-        a font ta funcionando?dsds
-      </div>
+    <div className="flex-col  w-full h-screen-minus-80 items-start bg-primary100 justify-items-center  ">
+      <h1 className="w-11/12 mt-11 text-white text-5xl text-left font-medium">
+        Home
+      </h1>
+      <nav className="flex flex-row items-center justify-between w-11/12 mt-10">
+        <div className="flex flex-row items-start ">
+          <Button
+            loading={false}
+            disabled={false}
+            onClick={() => console.log("wd")}
+            className="bg-primary200 font-medium text-lg  rounded text-center w-32 h-10 "
+            title="Atualizar"
+          />
+          <Button
+            loading={false}
+            onClick={() => console.log("wd")}
+            className=" bg-primary200 ml-12 font-medium text-lg  rounded text-center w-32  h-10 "
+            title="Baixar"
+          />
+        </div>
+        <div className="flex flex-row items-start ">
+          <input
+            type="text"
+            autoComplete="off"
+            placeholder="Pesquisar..."
+            className="w-80  py-2 px-3 h-10 rounded  mr-12  text-white border border-solid  border-primary200  focus:ease-in  focus:border-blue200 bg-primary100 transition-colors duration-300 focus:outline-none"
+          />
+          <Filter
+            options={data}
+            open={filter}
+            onClick={() => setFilter(!filter)}
+            selectedOptions={selectedOptions}
+            onSelect={handleSelect}
+          />
+        </div>
+      </nav>
+    </div>
   );
 }
