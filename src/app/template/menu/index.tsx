a7164591196bccb223d2615d52ad3abd15a71933
Arthur <arthurferreira1417@gmail.com>
2024-11-05 21:15:00 -0300

    Feat: create a menu componente

--- src/app/template/menu/index.tsx
+++ src/app/template/menu/index.tsx
@@ -1,4 +1,7 @@
+"use client";
 import Modal from "@/app/components/modal";
+import { Button } from "@/app/components/button";
+import { useState } from "react";
 interface IMenu {
   open: boolean;
   name: string;
@@ -7,10 +10,28 @@ interface IMenu {
 }
 
 export const Menu: React.FC<IMenu> = (props) => {
+  const [loading, setLoading] = useState(false);
+
   return (
     <Modal isOpen={props.open}>
-      <div onClick={props.close} className="flex items-center justify-center absolute  top-0 w-screen h-screen  bg-primary100/50 backdrop-blur-sm ">
-      SDSDSksmksdmksmdksdmksmksdksmdkksd
+      <div
+        onClick={props.close}
+        className="flex items-start justify-end absolute  top-0 w-screen h-screen  bg-primary100/50 backdrop-blur-0 "
+      >
+        <aside onClick={(e) => e.stopPropagation()}  className="flex flex-col items-center py-1 justify-around ml-auto mr-20 mt-16 w-40 h-28  rounded  bg-primary200">
+          <div className="w-4/5 flex flex-col items-start ">
+            <h1 className="text-left h-auto text-base text-white  font-medium">{props.name}</h1>
+            <h2 className="text-left text-xs h-auto mt-1 font-light text-white  ">{props.position}</h2>
+          </div>
+          <p className=" w-full h-[1px] bg-primary400"/>
+          <Button
+            loading={loading}
+            disabled={loading}
+            onClick={() => setLoading(!loading)}
+            className="h-7 bg-red100/70 font-medium text-sm  w-4/5 "
+            title={loading ? "" : "Sair"}
+          />
+        </aside>
       </div>
     </Modal>
   );
