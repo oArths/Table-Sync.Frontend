8d34aefa88a4b67237e104e3609e0fae57f926cf
Arthur <arthurferreira1417@gmail.com>
2024-11-05 21:14:30 -0300

    Feat: implementing menu changes

--- src/app/template/header/index.tsx
+++ src/app/template/header/index.tsx
@@ -2,35 +2,47 @@
 import SuspenseImage from "@/app/shared/suspenseImage";
 import { usePathname } from "next/navigation";
 import { Menu } from "../menu";
+import { useState } from "react";
 
 export const Header = () => {
+  const [menu, setMenu] = useState(false);
   let path = usePathname();
   if (path === "/") {
     return;
   }
 
   return (
-    <header className=" flex flex-row items-center justify-between w-full h-16 px-20  bg-primary100 border border-solid  border-b-gray100/50  ">
-      <div className=" w-[100px] h-[30px]">
-        <SuspenseImage
-          src="/LogoGrenke.webp"
-          alt="Logo da Grenke"
-          width={100}
-          height={30}
-          priority={true}
-        />
+    <header className="flex items-center justify-center w-full h-16   bg-primary100 border border-solid  border-b-gray100/50  ">
+      <div className="w-11/12  h-full flex flex-row items-center justify-between ">
+        <div className=" w-[100px] h-[30px]">
+          <SuspenseImage
+            src="/LogoGrenke.webp"
+            alt="Logo da Grenke"
+            width={100}
+            height={30}
+            priority={true}
+          />
+        </div>
+        <div
+          onClick={() => setMenu(!menu)}
+          className="cursor-pointer flex items-center justify-center rounded-full border border-solid border-primary200  h-11 w-11 bg-primary100"
+        >
+          <SuspenseImage
+            src="/UserIcon.svg"
+            alt="Logo da Grenke"
+            width={25}
+            className="rounded-full"
+            height={25}
+            priority={true}
+          />
+        </div>
       </div>
-      <div className="  cursor-pointer flex items-center justify-center rounded-full border border-solid border-primary200  h-11 w-11 bg-primary100">
-        <SuspenseImage
-          src="/UserIcon.svg"
-          alt="Logo da Grenke"
-          width={25}
-          className="rounded-full"
-          height={25}
-          priority={true}
-        />
-      </div>
-      <Menu name="clara" position="grenke" open={true} />
+      <Menu
+        name="Janaina Mendes"
+        position="Grenke"
+        close={() => setMenu(false)}
+        open={menu}
+      />
     </header>
   );
 };
