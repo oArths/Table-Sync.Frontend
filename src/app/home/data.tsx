98ecc30d3622c46d204ee5ec9f0882913fedb286
Arthur <arthurferreira1417@gmail.com>
2024-11-12 21:51:00 -0300

    Feat: implementing table with filter

---
+++ src/app/home/data.tsx
@@ -0,0 +1,1 @@
+export const Options = ["KU", "KA", "KL", "KD"];
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,32 +3,41 @@ import { useState } from "react";
 import { Button } from "../components/button";
 import Filter from "../components/dropdown/filter";
 import dynamic from "next/dynamic";
+import Table from "../template/table";
+import response from "../data/response.json"
+import { Options } from "./data";
+import {Data} from "../data/response.d"
 
 export default function Home() {
   const [filter, setFilter] = useState(false);
   const [download, setDownload] = useState(false);
   const [upload, setUpload] = useState(false);
-  const [selectedOptions, setSelectedOptions] = useState<string[]>(["KU"]);
-  const data = ["KU", "KA", "KL", "KD"];
+  const [selectedOptions, setSelectedOptions] = useState<string[]>(Options);
+  const [filteredData, setFilteredData] = useState(response);
+
 
   const handleSelect = (selected: string[]) => {
     setSelectedOptions(selected);
+
+    const Filtered = response.filter((item) => 
+      selected.includes(item.Cliente.Status) 
+    );
+    setFilteredData(Filtered);
+
   };
-  const Upload = dynamic(
-    () => import("../template/upload"),
-    {
-      loading: () => null,
-    }
-  );
-  const Download = dynamic(
-    () => import("../template/download"),
-    {
-      loading: () => null,
-    }
-  );
+  const Upload = dynamic(() => import("../template/upload"), {
+    loading: () => null,
+  });
+  const Download = dynamic(() => import("../template/download"), {
+    loading: () => null,
+  });
 
   return (
-    <div className="flex-col  w-full h-screen-minus-80 items-start bg-primary100 justify-items-center  ">
+    <div
+      className={`flex-col  w-full h-screen-minus-80   pb-20 items-start bg-primary100 justify-items-center  transition-all delay-200 ${
+       ( download || upload) && "overflow-y-hidden"
+      }`}
+    >
       <h1 className="w-11/12 mt-11 text-white text-5xl text-left font-medium">
         Home
       </h1>
@@ -56,7 +65,7 @@ export default function Home() {
             className="w-80  py-2 px-3 h-10 rounded  mr-12  text-white border border-solid  border-primary200  focus:ease-in  focus:border-blue200 bg-primary100 transition-colors duration-300 focus:outline-none"
           />
           <Filter
-            options={data}
+            options={Options}
             open={filter}
             onClick={() => setFilter(!filter)}
             selectedOptions={selectedOptions}
@@ -64,15 +73,9 @@ export default function Home() {
           />
         </div>
       </nav>
-          <Download
-          close={() => setDownload(!download)}
-          open={download}
-          
-          />
-          <Upload
-          close={() => setUpload(!upload)}
-          open={upload}
-          />
+      <Table data={filteredData} />
+      <Download close={() => setDownload(!download)} open={download} />
+      <Upload close={() => setUpload(!upload)} open={upload} />
     </div>
   );
 }
