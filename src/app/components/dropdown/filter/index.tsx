98e5f4f62719c759857611750b4ee96dd84443f7
Arthur <arthurferreira1417@gmail.com>
2024-11-14 16:31:38 -0300

    Feat: use props

--- src/app/components/dropdown/filter/index.tsx
+++ src/app/components/dropdown/filter/index.tsx
@@ -7,28 +7,22 @@ interface IFilter {
   onSelect: (selected: string[]) => void;
 }
 
-export default function Filter({
-  options,
-  open,
-  selectedOptions,
-  onClick,
-  onSelect,
-}: IFilter) {
+export default function Filter(props: IFilter) {
   const handleOptionClick = (value: string) => {
-    const isSelected = selectedOptions.includes(value);
-    if (isSelected && selectedOptions.length === 1) {
+    const isSelected = props.selectedOptions.includes(value);
+    if (isSelected && props.selectedOptions.length === 1) {
       return;
     }
     const newSelectedOptions = isSelected
-      ? selectedOptions.filter((option) => option !== value)
-      : [...selectedOptions, value];
+      ? props.selectedOptions.filter((option) => option !== value)
+      : [...props.selectedOptions, value];
 
-    onSelect(newSelectedOptions);
+    props.onSelect(newSelectedOptions);
   };
   return (
     <div className="relative  w-32 h-10  ">
       <p
-        onClick={onClick}
+        onClick={props.onClick}
         className={`cursor-pointer select-none font-medium text-base text-gray500 px-4 flex flex-row items-center justify-between w-full h-full bg-primary200 rounded`}
       >
         Status
@@ -37,18 +31,18 @@ export default function Filter({
           height="24px"
           color="rgb(144, 144, 144)"
           className={`  transition ease-linear  duration-200 ${
-            open ? "rotate-180 " : "rotate-0"
+            props.open ? "rotate-180 " : "rotate-0"
           }`}
         />
       </p>
-      {open && (
+      {props.open && (
         <div
           className={`absolute flex flex-col items-center justify-center py-1 z-10 w-32 mt-1 bg-primary300 rounded ${
-            open ? "animate-fadeInDown" : "animate-fadeOutUp"
+            props.open ? "animate-fadeInDown" : "animate-fadeOutUp"
           }`}
         >
-          {options.map((value, index) => {
-            const isSelected = selectedOptions.includes(value);
+          {props.options.map((value, index) => {
+            const isSelected = props.selectedOptions.includes(value);
             return (
               <p
                 key={index}
