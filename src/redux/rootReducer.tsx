4aaa26d615347103d77c1bfffdf2d13bed901b18
Arthur <arthurferreira1417@gmail.com>
2024-11-14 16:32:35 -0300

    Feat: update rootreducer

--- src/redux/rootReducer.tsx
+++ src/redux/rootReducer.tsx
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import menuSlice from "./menu/slice";
 
-const rootReducer = combineReducers({ menu: menuSlice });
+const rootReducer = combineReducers({ menu: menuSlice});
 export type RootState = ReturnType<typeof rootReducer>;
 
 export default rootReducer;
