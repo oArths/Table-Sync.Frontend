425d6f09ee8742f9fa455fe4b453dced4272a2a3
Arthur <arthurferreira1417@gmail.com>
2024-11-15 20:02:06 -0300

    Feat: remove header for layout to pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Tajawal } from "next/font/google";
-import { Header } from "./template/header/index";
 import StoreProvider from "./storeProvider";
 
 const tajawal = Tajawal({
@@ -26,10 +25,9 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body
         id="modal"
-        className={`${tajawal.variable} font-sans flex flex-col items-start h-full bg-primary100 `}
+        className={`${tajawal.variable} font-sans h-full bg-primary100 `}
       >
         <StoreProvider >
-          <Header />
           {children}
         </StoreProvider>
       </body>
