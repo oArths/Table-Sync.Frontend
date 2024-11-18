820b8a8325fa219c61dabb66b43851019286b885
Arthur <arthurferreira1417@gmail.com>
2024-11-18 02:35:34 -0300

    Feat: adding validation

--- src/redux/response/responseSelects.tsx
+++ src/redux/response/responseSelects.tsx
@@ -2,23 +2,25 @@ import { RootState } from "../rootReducer";
 
 export const simplifiedResponseSelector = (state: RootState) => {
   let response = state.response.response;
-
-  return response.map((Item) => ({
-    id: Item.id,
-    processNumber: Item.process.processNumber,
-    clientNumber: Item.client.clientNumber,
-    status: Item.client.status,
-    tenant: Item.client.tenant,
-    lastMovement: Item.process.lastMovement,
-    contractEnd: Item.contracts.contractEnd,
-    phase: Item.procedural.phase,
-    cnpj: Item.client.cnpj,
-  }));
+  if (response && Array.isArray(response)) {
+    return response.map((Item) => ({
+      id: Item.id,
+      processNumber: Item.process.processNumber,
+      clientNumber: Item.client.clientNumber,
+      status: Item.client.status,
+      tenant: Item.client.tenant,
+      lastMovement: Item.process.lastMovement,
+      contractEnd: Item.contracts.contractEnd,
+      phase: Item.procedural.phase,
+      cnpj: Item.client.cnpj,
+    }));
+  }
+  return null;
 };
 
 export const getContractSelector = (state: RootState, id: string) => {
   const item = state.response.response.find((item) => item.id === id);
-  console.log(item)
+  console.log(item);
   const exist = item ? item : null;
   return exist;
 };
