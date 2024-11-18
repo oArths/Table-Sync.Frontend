27ac3c38433f963ad33fddab0293c69045992eaf
Arthur <arthurferreira1417@gmail.com>
2024-11-18 02:34:10 -0300

    Feat: createing a response style

--- src/app/components/input/inputView/index.tsx
+++ src/app/components/input/inputView/index.tsx
@@ -1,12 +1,13 @@
 interface InputView {
   label: string;
   value: string | number;
+  className?: string;
 }
 export default function InputView(props: InputView) {
   return (
     <div className="flex flex-col gap-2 w-full items-start ">
       <label className="font-medium text-base  text-white">{props.label}</label>
-      <p className="h-10 rounded w-full p-2 text-sm text-start flex items-center justify-start border border-solid border-gray200">{props.value}</p>
+      <p className={` rounded w-full p-2 text-sm  border border-solid border-gray200  ${props.className ? props.className : 'flex items-center justify-start text-start  h-10'} `}>{props.value}</p>
     </div>
   );
 }
