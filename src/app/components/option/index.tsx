10fd756005fa3e650e21de2e0f82e0aec2803c39
Arthur <arthurferreira1417@gmail.com>
2024-11-21 15:20:10 -0300

    Feat: creating an icon when have error

--- src/app/components/option/index.tsx
+++ src/app/components/option/index.tsx
@@ -1,5 +1,5 @@
 import { ButtonHTMLAttributes } from "react";
-
+import * as I from "lucide-react"
 interface IOption extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
   active: boolean;
@@ -10,12 +10,12 @@ const Option: React.FC<IOption> = ({ label, active, hasError,...props }) => {
   return (
     <button
       {...props}
-      className={`w-1/5 text-center  flex items-center justify-center cursor-pointer select-none text-lg h-14 p-4 border border-solid transition-all delay-50 ease-in hover:border-b-white hover:text-white border-x-0 border-t-0 ${
+      className={`w-1/5 text-center  flex items-center justify-center  gap-2 cursor-pointer select-none text-lg h-14 p-4 border border-solid transition-all delay-50 ease-in hover:border-b-white hover:text-white border-x-0 border-t-0 ${
         active ? "border-b-white text-white" : "border-b-primary300 text-gray500"
-      } ${hasError && "border-b-red100 text-red100"}`}
+      } `}
       
     >
-      {label}
+      {label} {hasError && <I.Info size="14px" color="rgb(238, 18, 18)" />}
     </button>
   );
 };
