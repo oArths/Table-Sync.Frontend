6c7739c691809d609fc841631a7b0524964c10ff
Arthur <arthurferreira1417@gmail.com>
2024-11-22 19:07:29 -0300

    Feat: adding an nprogress-bar lib

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+productionBrowserSourceMaps: false,
   experimental: {
     optimizePackageImports: ['lucide-react', 'date-fns'],
   },
