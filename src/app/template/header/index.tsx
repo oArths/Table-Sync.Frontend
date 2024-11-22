3015ca8a077f5718f9b7a62cfc58a1b054da5ae5
Arthur <arthurferreira1417@gmail.com>
2024-11-22 19:09:44 -0300

    Feat: adding navigation to home

--- src/app/template/header/index.tsx
+++ src/app/template/header/index.tsx
@@ -4,48 +4,51 @@ import { useDispatch } from "react-redux";
 import { setStateMenu } from "@/redux/menu/slice";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/rootReducer";
+import { useRouter } from "next-nprogress-bar";
 
 export const Header = () => {
-  
+  const route = useRouter();
   const dispatch = useDispatch();
   const isOpen = useSelector((state: RootState) => state.menu.open);
   const handleClick = () => {
     dispatch(setStateMenu(!isOpen));
   };
+  const handleClickContract = () => {
+    route.push(`/home`);
+  };
 
-    return (
-      <header className="flex items-center justify-center w-full h-20 py-3 bg-primary100 border border-solid  border-b-gray100/50 border-x-0 border-t-0  ">
-        <div className="w-11/12  h-full flex flex-row items-center justify-between ">
-          <div className=" w-[100px] h-[30px]">
-            <SuspenseImage
-              src="/LogoGrenke.webp"
-              alt="Logo da Grenke"
-              width={100}
-              height={30}
-              priority={true}
-            />
-          </div>
-          <div
-            onClick={handleClick}
-            className="cursor-pointer flex items-center justify-center rounded-full border border-solid border-primary200  h-11 w-11 bg-primary100"
-          >
-            <SuspenseImage
-              src="/UserIcon.svg"
-              alt="Logo da Grenke"
-              width={25}
-              className="rounded-full"
-              height={25}
-              priority={true}
-            />
-          </div>
+  return (
+    <header className="flex items-center justify-center w-full h-20 py-3 bg-primary100 border border-solid  border-b-gray100/50 border-x-0 border-t-0  ">
+      <div className="w-11/12  h-full flex flex-row items-center justify-between ">
+        <div onClick={handleClickContract} className=" w-[100px] h-[30px] cursor-pointer">
+          <SuspenseImage
+            src="/LogoGrenke.webp"
+            alt="Logo da Grenke"
+            width={100}
+            height={30}
+            priority={true}
+          />
         </div>
-        <Menu
-          name="Janaina Mendes"
-          position="Grenke"
-          close={handleClick}
-          open={isOpen}
-        />
-      </header>
-    );
-  
+        <div
+          onClick={handleClick}
+          className="cursor-pointer flex items-center justify-center rounded-full border border-solid border-primary200  h-11 w-11 bg-primary100"
+        >
+          <SuspenseImage
+            src="/UserIcon.svg"
+            alt="Logo da Grenke"
+            width={25}
+            className="rounded-full"
+            height={25}
+            priority={true}
+          />
+        </div>
+      </div>
+      <Menu
+        name="Janaina Mendes"
+        position="Grenke"
+        close={handleClick}
+        open={isOpen}
+      />
+    </header>
+  );
 };
